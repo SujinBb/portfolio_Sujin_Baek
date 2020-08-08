ccd826f866228e9e096453e75d4487214fcdbf69
SujinBb <36348220+SujinBb@users.noreply.github.com>
2020-08-09 03:11:14 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   button = createButton('GO')
   button.mousePressed(LinkClik);
   button.position(680,420);
-  button.size(60,40);
+  button.size(50,40);
  button.style("font-family", "Impact");
   button.style("background-color","#fff");
  button.style("color","#000");
